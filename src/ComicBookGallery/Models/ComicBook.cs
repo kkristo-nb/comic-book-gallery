8bee5bf85027f24ff895c1ccfdf04851eeb6781a
Klaus <klaus.kristo@newbalance.com>
2018-02-14 14:44:14 -0500

    Added comic book repository

--- src/ComicBookGallery/Models/ComicBook.cs
+++ src/ComicBookGallery/Models/ComicBook.cs
@@ -9,7 +9,7 @@ namespace ComicBookGallery.Models
     {
         //Models can contain both attributes and behaviors
 
-        public int ID { get; set; }
+        public int Id { get; set; }
         public string SeriesTitle { get; set; }
         public int IssueNumber { get; set; }
         public string DescriptionHtml { get; set; }
