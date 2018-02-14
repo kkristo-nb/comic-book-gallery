db0d90d76c1fb963d206849e2d74312a1cdb2232
Klaus <klaus.kristo@newbalance.com>
2018-02-14 12:47:04 -0500

    Added comic book and artist models

---
+++ src/ComicBookGallery/Models/ComicBook.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ComicBookGallery.Models
+{
+    public class ComicBook
+    {
+        //Models can contain both attributes and behaviors
+
+        public int ID { get; set; }
+        public string SeriesTitle { get; set; }
+        public int IssueNumber { get; set; }
+        public string DescriptionHtml { get; set; }
+        public string[] Artists { get; set; }
+        public bool Favorite { get; set; }
+
+
+    }
+}
